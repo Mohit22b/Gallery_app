51f115cdca0bb62755ac610728f6010675877283
Mohit22b <mohit@bluebash.co>
2022-11-24 17:19:17 +0530

    forth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable        
   has_many :albums 
   after_create :Send_mail
-  
+    
   private
   def Send_mail
     UserMailer.with(user: self).welcome_email.deliver_now
